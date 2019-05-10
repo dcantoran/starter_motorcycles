88205d8308f865ab2b4025f340060fb0ce616ae0
github username <github email address>
2019-05-09 23:10:57 -0400

    first commit

---
+++ lib/starter_motorcycles.rb
@@ -0,0 +1,6 @@
+require "starter_motorcycles/version"
+
+module StarterMotorcycles
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/starter_motorcycles/version.rb
@@ -0,0 +1,3 @@
+module StarterMotorcycles
+  VERSION = "0.1.0"
+end
